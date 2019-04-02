efff6231a616640878e93e24e81bc33daa4b701b
fanrendale <1053314919@qq.com>
2019-04-02 17:01:47 +0800

    注册断路器指标流Servlet

---
+++ src/main/java/com/xjfe/eurekafeign/config/EurekaFeignConfig.java
@@ -0,0 +1,30 @@
+package com.xjfe.eurekafeign.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 应用配置类，初始化bean和配置信息
+ * @author xjf
+ * @date 2019/4/2 16:35
+ */
+@Configuration
+public class EurekaFeignConfig {
+
+    /**
+     * 注册断路器指标流Servlet
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+
+        return registrationBean;
+    }
+}
